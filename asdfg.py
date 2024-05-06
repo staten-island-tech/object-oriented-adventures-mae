b6fd4b91eff9f5192b9b3d2c001c5f6c3a8e0d5f
mariamikhutsishvili <mariamik2@nycstudents.net>
2024-05-06 14:00:18 -0400

    cxc

---
+++ asdfg.py
@@ -0,0 +1,10 @@
+class trix():
+    def __init__(info, name, power, damage):
+        info.name = name 
+        info.power = power
+        info.damage = damage 
+    def attack(self,char):
+        return char.health
+
+
+
--- enemies.py
+++ enemies.py
@@ -6,4 +6,14 @@ class trix():
 darcy=trix("Darcy","Darkness",100)
 stormy=trix("Stormy","Hurricane Sandy",100)
 icy=trix("Icy","Flurry Fury",100)
-print(goblin.__dict__)
+
+def attack(self,trix):
+    x = self.health-trix.damage
+    return x 
+
+
+def encounter_one():
+    print("You have encountered Darcy, one of the infamous trix sisters. She uses her power, Darkness, against you. YOU JUST LOST 100 HEALTH POINTS! GET WRECKED.")
+    print("Your helath is at 400")
+    
+encounter_one()
