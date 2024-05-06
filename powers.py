638d423c432f49c8ca1af3d2a9a3183a780a6120
agnesl10 <158211772+agnesl10@users.noreply.github.com>
2024-05-06 13:59:42 -0400

    Update powers.py

--- powers.py
+++ powers.py
@@ -1,7 +1,14 @@
-class Powers():
-    def __init__(info, name, power, damage):
-        info.name = name
-        info.damage = damage
+class powers():
+    def __init__(info, power, damage):
         info.power = power
+        info.damage = damage
 
-nature = Powers("")
+nature = powers("Deforestation", 100)
+fire = powers("Volcano Boom", 120)
+water = powers("Hurricane-y Spin-y", 110)
+light = powers("Sun Burnt Oven", 100)
+music = powers("Deafening Speakers", 101)
+
+def choosing_power():
+    x = input("Now that you just recieved that blow like a stupid dummy. Attack back. Which element would you like to utilize? (n/f/w/l/m)").lower()
+    if x 
