53810c0a839ce31d9043a28e6eb89b9e4e045b75
mariamikhutsishvili <mariamik2@nycstudents.net>
2024-05-07 13:59:20 -0400

    dd

--- enemies.py
+++ enemies.py
@@ -7,13 +7,18 @@ darcy=trix("Darcy","Darkness",100)
 stormy=trix("Stormy","Hurricane Sandy",100)
 icy=trix("Icy","Flurry Fury",100)
 
-def attack(self,trix):
-    x = self.health-trix.damage
+def attack(user,trix):
+    x = user.health-trix.damage
     return x 
 
 
 def encounter_one():
     print("You have encountered Darcy, one of the infamous trix sisters. She uses her power, Darkness, against you. YOU JUST LOST 100 HEALTH POINTS! GET WRECKED.")
-    print("Your helath is at 400")
-    
-encounter_one()
+    print("Your health is at 400")
+
+def encounter_two():
+    print("Good job! Let's continue on out journey. OH NO!!! ITS STORMY. She used her power, Hurricane Sandy, against you. YOU LOST 100 HEALTH POINTS!")
+    print("Your health is at 300")
+
+def encounter_three():
+    print("Congrats on getting this far! TIME FOR BOSS LEVEL, HERE COMES ICY. Fire first...")
