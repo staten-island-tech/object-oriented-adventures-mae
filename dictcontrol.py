24c7eae6afeb1b46b41e629b692775b3818caa77
eviekarimov <evelynk23@nycstudents.net>
2024-05-07 13:58:30 -0400

    gdrhhrsjthea

--- dictcontrol.py
+++ dictcontrol.py
@@ -1,5 +1,9 @@
-class hero(name):
+class hero():
     def__init__(name, )
-   
-info.name
-        name = input("bloom: ")
+        info.name
+        
+        
+name = input("bloom: ")
+
+info.character
+character = 
