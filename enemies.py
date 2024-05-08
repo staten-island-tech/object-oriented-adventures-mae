4cf27fb0a068bb1c16fd3be406809dc26285f060
mariamikhutsishvili <mariamik2@nycstudents.net>
2024-05-08 13:59:04 -0400

    dd

--- enemies.py
+++ enemies.py
@@ -11,6 +11,8 @@ def attack(user,trix):
     x = user.health-trix.damage
     return x 
 
+class encounters():
+    
 
 def encounter_one():
     print("You have encountered Darcy, one of the infamous trix sisters. She uses her power, Darkness, against you. YOU JUST LOST 100 HEALTH POINTS! GET WRECKED.")
@@ -21,4 +23,9 @@ def encounter_two():
     print("Your health is at 300")
 
 def encounter_three():
-    print("Congrats on getting this far! TIME FOR BOSS LEVEL, HERE COMES ICY. Fire first...")
+    y = input("Congrats on getting this far! TIME FOR BOSS LEVEL, HERE COMES ICY. Fire first by inputing fire: ").lower()
+    if y == ("fire"):
+        print(f"{500 - character['power'].damage} is icy's health. She fired back at you but she missed! Lucky you...:/ I guess it's your turn again.@#%$#!@")
+    w = input("Input attack to fire: ").lower()
+    if w == ("attack"):
+        print("NO FLIPPING WAY, YOU WON SOMEHOW. Your attack blinded icy and gave her ptsd. dont ask questions :)")
