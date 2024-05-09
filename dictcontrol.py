2da69674d6897ed2cb84c26b31561d1d5bc73955
eviekarimov <evelynk23@nycstudents.net>
2024-05-09 13:51:26 -0400

    ut65rifjyrtd

--- dictcontrol.py
+++ dictcontrol.py
@@ -1,27 +1,26 @@
 class character():
-    def __init__(self, name, gender, element):
+    def __init__(self, name,age, gender, element):
         self.name = name
         self.gender = gender
-        self.element = {}
+        self.age =age
+        self.element = element
 
     def display_info(self):
         print(f"Name: {self.name}")
         print(f"Age: {self.age}")
         print(f"Gender: {self.gender}")
         print(f"Element: {self.element}")
-        
-    def create_character():
-        name = input("Enter character name: ")
-        age = int(input("Enter character age: "))
-        gender = input("Enter characters gender: ")
-        element = input("Choose characters element (fire, water, earth, air): ")
-        return Character(name, age, gender, element)
+    
 
-    def main():
-        print("Welcome to Character Creation!")
-        character = create_character()
-        character.display_info()
+def create_character():
+    name = input("Enter character name: ")
+    age = int(input("Enter character age: "))
+    gender = input("Enter characters gender: ")
+    element = input("Choose characters element (fire, water, earth, air): ")
+    return character(name, age, gender, element)
 
-    create_chracater().display_info(
+User = create_character()
+print(User.display_info())
+
 
         
