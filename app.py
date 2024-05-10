78a42641e9dff532ccb8489ad644b984a52d676f
agnesl10 <158211772+agnesl10@users.noreply.github.com>
2024-05-10 13:56:02 -0400

    sdfsdf

---
+++ app.py
@@ -0,0 +1,8 @@
+from enemies import trix, encounters
+
+t = trix("sdafg", "air", 56)
+darcy=trix("Darcy","Darkness",100)
+stormy=trix("Stormy","Hurricane Sandy",100)
+icy=trix("Icy","Flurry Fury",100)
+print(t.name)
+
