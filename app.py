41584907c2d881461a8efe62f1af1fd4c7bc4cbe
eviekarimov <evelynk23@nycstudents.net>
2024-06-03 13:36:24 -0400

    rgrdszygSDRZh

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 print("> Hello. Welcome to Mr. Whalen's Adventure World. It is literally the best thing in the world. You are going to enter this weird world and fight three bratty women. Easiest game, yea...")
 print("> Wait..uhh...who are you?")
 from characters import *
-from powers import * 
+from powers import *
 from enemies import *
-import time
 #making character
-user = character()
+user = character.create_character()
 q = input("> Check if the information you above is correct. (y/n) ").lower()
 if q == "y":
     print("> Alright then. Let's continue and toss you in there.")
@@ -16,29 +15,20 @@ elif q == "n":
 
 #entering world
 print("> Hey. You awake? You hit your head pretty hard when I tossed you. I really don't care. Anyway...")
-time.sleep(3)
 print("> You see that road. No? Imagine it. Uh, follow it and try to use that new power of yours.")
-time.sleep(3)
 print("> Ever heard of the trix? Three girls, awful style - no? me neither. They really wanna kill you. Have FUN!!")
-time.sleep(3)
 encounters.one()
-<<<<<<< HEAD
-a = input(f"> You used {user.element['power']}! You took {user.element['damage']} off of Darcy. {300 - user.element['damage']}.Congrats, newbie. WAIT. Don't celebrate yet... Watch out, there's more coming for you. Are you sure you want to continue? Your life depends on it... (y/n): ").lower()
-=======
-a = input(f"> You used {user.element['power']}! You took {user.element['damage']} off of Darcy. {300 - user.element['damage']}.Congrats, newbie. WAIT. Don\'t celebrate yet... Watch out, there\'s more coming for you. Are you sure you want to continue? Your life depends on it... (y/n): ").lower()
->>>>>>> 4069eebd148f3b8eb4010d21960da9cc8e1dfbb5
+a = input(f"> You used {user["element"].power}! You took {user["element"].damage} off of Darcy. {300 - user["element"].damage}.Congrats, newbie. WAIT. Don't celebrate yet... Watch out, there's more coming for you. Are you sure you want to continue? Your life depends on it... (y/n): ").lower()
 if a == "y":
     print("> Ok, if you say so...")
 elif a == "n":
     print("> BRUHHHH, BOY BYE")
 encounters.two()
-<<<<<<< HEAD
-b = input(f"> You used {user.element['power']}! You took {user.element['damage']} off of Stormy. {300 - user.element['damage']}. Almost there buddy boy, do you wish to continue? (y/n): ").lower()
-=======
-b = input(f"> You used {user.element['power']}! You took {user.element['damage']} off of Stormy. {300 - user.element['damage']}. Her ego is so big she ran away crying. Almost there buddy boy, do you wish to continue? (y/n): ").lower()
->>>>>>> 4069eebd148f3b8eb4010d21960da9cc8e1dfbb5
+b = input(f"> You used {user["element"].power}! You took {user["element"].damage} off of Stormy. {300 - user["element"].damage}. Almost there buddy boy, do you wish to continue? (y/n): ").lower()
 if b == "y":
     print("> THATS WHAT IM TALKING ABOUT")
 if b == "n":
-    print("> Flipping flipers")
+    print("> Flipping flippers")
 encounters.three()
+
+
